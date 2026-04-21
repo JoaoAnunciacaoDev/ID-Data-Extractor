34f7dbce48829ca86e94afcf749af381b8085c76
João Anunciação <js5283283@gmail.com>
2026-04-21 12:48:08 -0300

    Adicionando o código VBA para automação

--- exporter.py
+++ exporter.py
@@ -8,7 +8,6 @@ class CsvExporter:
             raise ValueError("Não há dados para exportar.")
             
         with open(output_path, "w", newline="", encoding="utf-8-sig") as f:
-            # BASTA ADICIONAR O DELIMITER AQUI:
             writer = csv.DictWriter(f, fieldnames=["foto", "nome", "rg", "data_nascimento"], delimiter=';')
             writer.writeheader()
             writer.writerows(data_list)
